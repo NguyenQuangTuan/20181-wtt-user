1be1b668d2ec6fdefdff0066654fb2130a078fd7
NguyenQuangTuan <quangtuan0701@gmail.com>
2018-12-07 07:53:38 +0700

    fix config

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ module.exports = {
 		username: 'wtt',
 		password: 'Wtt@20181',
 		database: 'wtt-user',
-		host: '18.191.51.82',
+		host: '18.223.166.127',
 		dialect: 'mysql',
 		dialectOptions: { charset: 'utf8mb4', decimalNumbers: true },
 		define: { charset: 'utf8mb4', collate: 'utf8mb4_unicode_ci' },
@@ -15,7 +15,7 @@ module.exports = {
 		}
 	},
 	elasticsearch: {
-		host: '18.191.172.251:9200',
+		host: '18.223.166.127:9200',
 		log: 'trace',
 		index_user: 'wtt_user_dev',
 		type: '_doc'
